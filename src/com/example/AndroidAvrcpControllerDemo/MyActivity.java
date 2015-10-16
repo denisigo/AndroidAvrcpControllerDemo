5082172ae674431c6ddfd034bce2b3ab20c7e9ec
denisigo <denisigo@gmail.com>
2015-10-16 23:31:53 +0300

    Added support for get element attrs to AVRC CT

--- src/com/example/AndroidAvrcpControllerDemo/MyActivity.java
+++ src/com/example/AndroidAvrcpControllerDemo/MyActivity.java
@@ -3,6 +3,8 @@ package com.example.AndroidAvrcpControllerDemo;
 import android.app.Activity;
 import android.bluetooth.*;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -15,12 +17,14 @@ public class MyActivity extends Activity {
 
     private BluetoothAvrcpController mAvrcpController;
     private TextView mStatusView;
+    private TextView mAttrsView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mStatusView = (TextView)findViewById(R.id.status);
+        mAttrsView = (TextView)findViewById(R.id.attrs);
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothAdapter.getProfileProxy(this, mAvrcpServiceListener, BluetoothProfile.AVRCP_CONTROLLER);
@@ -31,6 +35,7 @@ public class MyActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mAvrcpController.removeCallback();
         bluetoothAdapter.closeProfileProxy(BluetoothProfile.AVRCP_CONTROLLER, mAvrcpController);
     }
 
@@ -42,6 +47,7 @@ public class MyActivity extends Activity {
                 Log.d(TAG, "AvrcpControllerService connected");
 
                 mAvrcpController = (BluetoothAvrcpController) proxy;
+                mAvrcpController.setCallback(new AvrcpControllerCallback());
 
                 mStatusView.append("\r\nAvrcp devices: \r\n");
                 List<BluetoothDevice> devices = mAvrcpController.getConnectedDevices();
@@ -55,7 +61,7 @@ public class MyActivity extends Activity {
             if (profile == BluetoothProfile.AVRCP_CONTROLLER) {
                 mStatusView.setText("AVRCP_CONTROLLER disconnected");
                 Log.d(TAG, "AvrcpControllerService disconnected");
-
+                mAvrcpController.removeCallback();
                 mAvrcpController = null;
             }
         }
@@ -92,4 +98,61 @@ public class MyActivity extends Activity {
     public void onPrevButtonClick(View view){
         sendCommand(BluetoothAvrcp.PASSTHROUGH_ID_BACKWARD);
     }
+
+    public void onGetAttrsButtonClick(View view){
+        if (mAvrcpController == null)
+            return;
+
+        int[] attrs = {BluetoothAvrcp.MEDIA_ATTR_TITLE,
+                BluetoothAvrcp.MEDIA_ATTR_ARTIST,
+                BluetoothAvrcp.MEDIA_ATTR_ALBUM};
+        List<BluetoothDevice> devices = mAvrcpController.getConnectedDevices();
+        for (BluetoothDevice device : devices){
+            Log.d(TAG, "send getAttrs to device: " + device.getName() + " " + device.getAddress());
+            mAvrcpController.getElementAttr(device, attrs.length, attrs);
+        }
+    }
+
+    private class AvrcpControllerCallback extends IBluetoothAvrcpControllerCallback.Stub {
+
+        private static final String TAG = "AvrcpControllerCallback";
+
+        @Override
+        public IBinder asBinder() {
+            return this;
+        }
+
+        @Override
+        public void onGetElementAttrRsp(final int numAttr, final int[] attrs, final String[] values) throws RemoteException {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mAttrsView.setText("");
+                    for (int i = 0; i < numAttr; i++) {
+                        if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_TITLE)
+                            mAttrsView.append("Title: " + values[i] + "\r\n");
+                        else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_ARTIST)
+                            mAttrsView.append("Artist: " + values[i] + "\r\n");
+                        else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_ALBUM)
+                            mAttrsView.append("Album: " + values[i] + "\r\n");
+                        /*else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_GENRE)
+                            mAttrsView.append("Genre: " + values[i] + "\r\n");
+                        else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_NUM_TRACKS)
+                            mAttrsView.append("Num tracks: " + values[i] + "\r\n");
+                        else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_TRACK_NUM)
+                            mAttrsView.append("Track num: " + values[i] + "\r\n");
+                        */
+                        else if (attrs[i] == BluetoothAvrcp.MEDIA_ATTR_PLAYING_TIME)
+                            mAttrsView.append("Playing time: " + values[i] + " ms \r\n");
+
+                        Log.d(TAG, "onGetElementAttrRsp attr_id:" + attrs[i] + " value: " + values[i]);
+                    }
+                }
+            });
+
+        }
+    }
+
+
+
 }
